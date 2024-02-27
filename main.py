f3803c95ff74d2c44c2824a52bed950d12e40e75
Rully Agus Hendrawan <ruhendrawan@pitt.edu>
2024-02-26 19:56:38 -0500

    starting class

---
+++ main.py
@@ -0,0 +1,9 @@
+class Destination:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.best_weather = "sunny"
+        self.season = "summer"
+        self.distance_from_upitt = 0
+
+
