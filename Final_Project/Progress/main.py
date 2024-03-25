45c73f6c5b5b981613804e26148bcbadc979ac7c
Tr1ee <tr1ee@Tr1eedeMacBook-Air.local>
2024-03-25 19:40:02 -0400

    update

--- Final_Project/Progress/main.py
+++ Final_Project/Progress/main.py
@@ -1,13 +1,11 @@
 import pandas as pd
 
 class NationalPark: 
-    def init(self, id, name, longitude, latitude): 
-        self.id = id 
-        self.name = name 
-        self.longitude = longitude 
-        self.latitude = latitude 
-        self.visitors = 0
-        self.weather = [] 
+    def __init__(self, name, location, visitors=0):
+        self.name = name
+        self.location = location
+        self.visitors = visitors
+        self.weather = []
         self.expenses = []
 
     def add_visitor(self, visitor):
@@ -20,36 +18,45 @@ class NationalPark:
         self.expenses.append(expense)
     
 class Visitors: 
-    def init(self, year, month, number_of_visitors): 
+    def __init__(self, year, month, number_of_visitors): 
         self.year = year 
         self.month = month 
-        
         self.number_of_visitors = number_of_visitors
 
 class Weather: 
-    def init(self, year, month, day, weather): 
+    def __init__(self, year, month, day, weather): 
         self.year = year 
         self.month = month 
         self.day = day 
         self.weather = weather
 
 class Expense: 
-    def init(self, destination_name, travel_vehicle, cost): 
+    def __init__(self, destination_name, travel_vehicle, cost): 
         self.destination_name = destination_name 
         self.travel_vehicle = travel_vehicle 
         self.cost = cost
 
 class ParkInfo: 
-    def init(self, terrain, climate, crowds): 
+    def __init__(self, terrain, climate, crowds): 
         self.terrain = terrain 
         self.climate = climate 
         self.crowds = crowds
 
-def read_excel_file(file_path): 
-    national_parks = [] 
-    df = pd.read_excel(file_path) 
-    for index, row in df.iterrows(): 
-        park_name = row['ParkName'] 
-        latitude = row['Location'] 
-        longitude = row['NumberOfVisitors'] 
+def read_excel_file(file_path):
+    national_parks = []
+    df = pd.read_excel(file_path)
+    for index, row in df.iterrows():
+        park_name = row['ParkName']
+        location = row['Location']
+        visitors = row['NumberOfVisitors']
+        national_park = NationalPark(park_name, location, visitors)
+        national_parks.append(national_park)
     return national_parks
+
+def print_park_info(national_parks):
+    for park in national_parks:
+        print(f"Name: {park.name}, Location: {park.location}, Visitors: {park.visitors}")
+
+file_path = '/Users/tr1ee/PythonFinal/Final_Project/Progress/National_Parks_Visitors.xlsx'
+national_parks = read_excel_file(file_path)
+print_park_info(national_parks)
